9296339d501309a24f906cbc785778363e60f17b
diego <diego@diegogurgel.com>
2014-05-25 02:37:51 -0300

    add config button

--- js/main.js
+++ js/main.js
@@ -1,14 +1,29 @@
 window.onload = function(){
-	console.log("ok");
-	var btnDrawer = document.querySelector(".btn-drawer");
-	var drawerPainel = document.getElementsByTagName('aside')[0];
+	
+	var btnDrawer = document.querySelector(".drawer-icon");
+	var btnConfig = document.querySelector(".btn-config");
+	console.log(btnConfig);
+	btnDrawer.onclick = togglePainel;
+	btnConfig.onclick = toggleConfig;
 
-	btnDrawer.onclick = function(){
-		if(drawerPainel.className=="hide"){
-			drawerPainel.className = "";
-		}else{
-			drawerPainel.className = "hide";
-		}
-		
+}
+
+function togglePainel () {
+	var drawerPainel = document.getElementsByTagName('aside')[0];
+	if(drawerPainel.className=="hide"){
+		drawerPainel.className = "";
+	}else{
+		drawerPainel.className = "hide";
 	}
+
+}
+function toggleConfig(){
+	console.log("ok2");
+	var configPainel = document.querySelector('.config-container');
+	if(configPainel.className=="config-container hide-config"){
+		configPainel.className = "config-container";
+	}else{
+		configPainel.className = "config-container hide-config";
+	}
+
 }
